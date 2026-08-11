0b9efbd701690df585e8f06b25772e7fc0fc04c8
Connor Johnston <connorallen0716@gmail.com>
2026-08-10 21:52:13 -0400

    fix headline line breaks

--- components/site/Hero.tsx
+++ components/site/Hero.tsx
@@ -21,7 +21,7 @@ export default function Hero() {
             Serving all of Ocean County, NJ
           </div>
           <h1 className="text-5xl sm:text-6xl lg:text-7xl font-extrabold tracking-tight leading-[1.1] drop-shadow-[2px_3px_4px_rgba(0,0,0,0.7)]">
-            Ocean County&apos;s<br /><span className="text-[#f47820] drop-shadow-[3px_4px_6px_rgba(0,0,0,0.9)]">fastest</span> junk<br />removal service.
+            Ocean County&apos;s <span className="text-[#f47820] drop-shadow-[3px_4px_6px_rgba(0,0,0,0.9)]">fastest</span><br />junk removal service.
           </h1>
 
           <motion.div
